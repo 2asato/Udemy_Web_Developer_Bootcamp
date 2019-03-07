ca518e144496ca032077860358711963afd1feb7
aaronsato <2asato@gmail.com>
2019-03-07 11:41:15 -0800

    completed stalkerExercise

---
+++ Javascript/stalkerExercise/script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("Hi, what is your first name?");
+
+var lastName = prompt("Cool, what is your last name?");
+
+var age = prompt("Nice to meet you, how old are you?");
+
+console.log("Nice to meet you " + firstName + " " + lastName);
+
+console.log(firstName + " " + lastName + " is " + age + " years old.");
