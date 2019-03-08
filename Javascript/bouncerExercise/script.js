0b48dfaa689c243487762afcf786c84f2ebc536d
aaronsato <2asato@gmail.com>
2019-03-08 00:17:47 -0800

    completed bouncer exercise

---
+++ Javascript/bouncerExercise/script.js
@@ -0,0 +1,17 @@
+var age = prompt("How old are you?");
+
+if (age < 0) {
+  console.log("ERROR!!!");
+}
+
+if (age == 21) {
+  console.log("happy 21st birthday!!");
+}
+
+if (age % 2 == 1) {
+  console.log("your age is odd!");
+}
+
+if (Number.isInteger(Math.sqrt(age))) {
+  console.log("perfect square");
+}
