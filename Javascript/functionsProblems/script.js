77f8d6379feaaaf64e44025bc7832bcd4ea42878
aaronsato <2asato@gmail.com>
2019-03-11 15:21:28 -0700

    created and set up function problems

---
+++ Javascript/functionsProblems/script.js
@@ -0,0 +1,7 @@
+// write a function which takes a single numeric argument and returns true if the number is even and false otherwise
+
+
+// write a function which takes a single numeric argument and returns the factorial of that number
+
+
+// write a function which takes a single kebab-cased string and returns the snake_cased version
