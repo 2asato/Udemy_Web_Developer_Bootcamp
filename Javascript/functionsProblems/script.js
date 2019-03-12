49974f353e421062d9732d5b49d0e7800c48cb98
aaronsato <2asato@gmail.com>
2019-03-11 23:31:46 -0700

    completed functionProblems

--- Javascript/functionsProblems/script.js
+++ Javascript/functionsProblems/script.js
@@ -1,7 +1,28 @@
 // write a function which takes a single numeric argument and returns true if the number is even and false otherwise
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+// short and sweet
+function isEven(num) {
+  return num % 2 === 0;
+}
 
 
 // write a function which takes a single numeric argument and returns the factorial of that number
+function factorial(num) {
+  for (let i = num-1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
 
 
 // write a function which takes a single kebab-cased string and returns the snake_cased version
+function kebabToSnake(str) {
+  let newStr = str.replace("-", "_")
+  return newStr;
+}
