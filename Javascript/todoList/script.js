cb551e81e0a5ddb378ccd80fc2ddbce0de0dc95a
aaronsato <2asato@gmail.com>
2019-03-13 00:09:39 -0700

    more todoList

--- Javascript/todoList/script.js
+++ Javascript/todoList/script.js
@@ -1,18 +1,46 @@
-var todos = ["Buy New Turtle"];
+window.setTimeout(function() {
+  var todos = ["Buy New Turtle"];
 
-var input = prompt("What would you like to do?")
+  var input = prompt("What would you like to do?")
 
-while(input !== "quit") {
-  // handle input
-  if(input === "list") {
-    console.log(todos);
-  } else if (input === "new") {
-  // ask for new todo
-  var newTodo = prompt("Enter new todo")
-  // add to todos array
-  todos.push(newTodo);
+  while(input !== "quit") {
+    // handle input
+    if(input === "list") {
+      listTodos();
+    } else if (input === "new") {
+      newTodos();
+    } else if (input === "delete") {
+      deleteTodos();
+    }
+    // ask again for new input
+    input = prompt("What would you like to do?")
   }
-  // ask again for new input
-  input = prompt("What would you like to do?")
-}
-console.log("OK, YOU QUIT THE APP");
+  console.log("OK, YOU QUIT THE APP");
+
+
+  function listTodos() {
+    console.log("**********");
+    todos.forEach(function(todo, index){
+      console.log(index + ": " + todo);
+    });
+    console.log("**********");
+  }
+
+  function newTodos() {
+    // ask for new todo
+    var newTodo = prompt("Enter new todo")
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+  }
+
+  function deleteTodos() {
+    // ask for index of todo to be deleted
+    var index = prompt("Enter the index of todo to delete");
+    // delete that todo
+    // splice
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+  }
+
+}, 500);
