396f0d2c721502af5e961bd2350a80a7c307575c
aaronsato <2asato@gmail.com>
2019-03-16 22:48:39 -0700

    selectors exercise complete

---
+++ theDOM/selectorsExercise/script.js
@@ -0,0 +1,14 @@
+// return first p element
+
+var firstPId = document.getElementById("first")
+
+var firstPQuery = document.querySelector("#first")
+
+var firstPClass = document.getElementsByClassName("special")[0]
+
+var firstPClassQuery = document.querySelector(".special")
+                       document.querySelector("p")
+
+var firstPClassQueryAll = document.querySelector(".special")[0]
+
+var firstPTag = document.getElementsByTagName("p")[0]
