bd5996d372b6993a07342f5043be5255481e0f3e
aaronsato <2asato@gmail.com>
2019-03-18 12:40:15 -0700

    completed

---
+++ theDOM/googleCodeAlong/script.js
@@ -0,0 +1,16 @@
+// grab the google logo
+var newImg = document.querySelector("img");
+
+// change logo
+newImg.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR_Fg_lMrPjMa1duHXnvFnZOtsn883LGLD7c2-CwKdfFXAoveQnQ")
+
+// change new logo to be same size as old
+newImg.style.width = "525px"
+newImg.style.height = "500px"
+
+// using a loop to change a bunch of same element
+var links = document.getElementsByTagName("a")
+for (let i = 0; i < links.length; i++) {
+  // console.log(links[i].textContent);
+  links[i].style.background = "pink"
+}
---
+++ theDOM/manipulating/script.js
@@ -0,0 +1,44 @@
+// .style
+// manipulate element's style
+
+// tag.style.color = "blue";
+// styles the element "tag" with the color blue
+
+
+// .classList
+// allows you to add multiple styles with just a single class to stay away from DRY
+
+// add a class
+// tag.classList.add("class-name")
+
+// remove a class
+// tag.classList.remove("class-name")
+
+// toggle a class
+// tag.classList.toggle("class-name")
+
+
+// .textContent
+// returns a string of all text contained in a given element
+
+// retrieve textContent
+// tag.textContent returns text in tag
+
+// alter text
+// tag.textContent = "alfjlds;fjlsdafj"
+
+
+// .innerHTML
+// returns a string of all HTML contained in a given element
+
+// tag.innerHTML returns ex. "This is an <strong>awesome</strong> paragraph"
+
+
+// .getAttribute
+// to read and write attributes lide src or href
+
+// link.getAttribute("href") returns href of link ex. www.google.com
+
+// change attribute - works the same with src or any other attribute
+// link.setAttribute("href", "www.dogs.com")
+// changes href from www.google.com to www.dogs.com
