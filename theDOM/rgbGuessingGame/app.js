4e1156b2f07afd5fccb325f9e435b6b2d3630b26
aaronsato <2asato@gmail.com>
2019-03-20 16:37:44 -0700

    completed part 4

--- theDOM/rgbGuessingGame/app.js
+++ theDOM/rgbGuessingGame/app.js
@@ -1,12 +1,5 @@
 // set colors for game
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 // grab squares
 var squares = document.querySelectorAll(".square");
@@ -14,6 +7,8 @@ var squares = document.querySelectorAll(".square");
 var rgbDisplay = document.querySelector("#rgbDisplay");
 // grab message
 var message = document.querySelector("#message");
+// grab h1
+var h1 = document.querySelector("h1");
 
 // set pickedColor
 var pickedColor = pickColor();
@@ -33,6 +28,8 @@ for (let i = 0; i < squares.length; i++) {
     if (clickedColor === pickedColor) {
       message.textContent = "Correct!!!";
       changeColors(clickedColor);
+      // changes h1 color on correct guess
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       message.textContent = "Try Again!"
@@ -51,4 +48,27 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(numOfColors) {
+  // make an array
+  var randomColorsArr = [];
+  // add numOfColors random colors to array
+  for (let i = 0; i < numOfColors; i++) {
+    // get random color and push into array
+    randomColorsArr.push(randomColor());
+  }
+  // return that array
+  return randomColorsArr;
+}
+
+function randomColor() {
+  // pick a red from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  // pick a green from 0 -255
+  var g = Math.floor(Math.random() * 256);
+  // pick a blue from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+  // console.log(rgb);
 }
