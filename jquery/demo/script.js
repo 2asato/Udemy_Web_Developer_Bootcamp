cc636b0219d66c59457c7b2db697c2c50edacef6
aaronsato <2asato@gmail.com>
2019-04-12 13:59:19 -0700

    selector exercise folder setup

--- jquery/demo/script.js
+++ jquery/demo/script.js
@@ -1,2 +1,27 @@
 // $("selectorGoesHere")
 // similar to querySelectorAll
+
+// to select all img tags
+// $("img")
+
+// to select all elements with class sale
+// $(".sale")
+
+// to select all a tags inside of li's
+// $("li a")
+
+
+// .css(property, value)
+$("h1").css("color", "yellow");
+
+// w/o jquery
+// document.querySelector("h1").style.color = "yellow"
+
+// changing multiple styles
+var styles = {
+  color: "red",
+  background: "pink",
+  border: "2px solid purple"
+}
+
+$("h1").css(styles);
