1d80087d34cae1ea4aceb032f91153c27f6e493a
aaronsato <2asato@gmail.com>
2019-06-13 23:35:27 -0700

    added another jquery method to click todos

--- jquery/todoList/assets/javascript/todos.js
+++ jquery/todoList/assets/javascript/todos.js
@@ -1,21 +1,21 @@
 // Check off specific todos by clicking
-$('li').click(function(){
-    $(this).toggleClass('completed');
-});
-// without class
 // $('li').click(function(){
-//     // if li is gray turn it black if black turn it gray
-//     if($(this).css('color') === 'rgb(128, 128, 128)') {
-//         // turn it black, no line through
-//         $(this).css({
-//             color: 'black',
-//             textDecoration: 'none'
-//         })
-//     }
-//     else {
-//         $(this).css({
-//             color: 'grey',
-//             textDecoration: 'line-through'
-//         })
-//     }
+//     $(this).toggleClass('completed');
 // });
+// without class
+$('li').click(function(){
+    // if li is gray turn it black if black turn it gray
+    if($(this).css('color') === 'rgb(128, 128, 128)') {
+        // turn it black, no line through
+        $(this).css({
+            color: 'black',
+            textDecoration: 'none'
+        })
+    }
+    else {
+        $(this).css({
+            color: 'grey',
+            textDecoration: 'line-through'
+        })
+    }
+});
