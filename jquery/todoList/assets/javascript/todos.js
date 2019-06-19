cfc5c3984364b7e0597dae401a41a33bfba7428f
aaronsato <2asato@gmail.com>
2019-06-19 16:02:26 -0700

    remove todo on click of span

--- jquery/todoList/assets/javascript/todos.js
+++ jquery/todoList/assets/javascript/todos.js
@@ -1,21 +1,33 @@
 // Check off specific todos by clicking
-// $('li').click(function(){
-//     $(this).toggleClass('completed');
-// });
-// without class
 $('li').click(function(){
-    // if li is gray turn it black if black turn it gray
-    if($(this).css('color') === 'rgb(128, 128, 128)') {
-        // turn it black, no line through
-        $(this).css({
-            color: 'black',
-            textDecoration: 'none'
-        })
-    }
-    else {
-        $(this).css({
-            color: 'grey',
-            textDecoration: 'line-through'
-        })
-    }
+    $(this).toggleClass('completed');
 });
+// without class
+// $('li').click(function(){
+//     // if li is gray turn it black if black turn it gray
+//     if($(this).css('color') === 'rgb(128, 128, 128)') {
+//         // turn it black, no line through
+//         $(this).css({
+//             color: 'black',
+//             textDecoration: 'none'
+//         })
+//     }
+//     else {
+//         $(this).css({
+//             color: 'grey',
+//             textDecoration: 'line-through'
+//         })
+//     }
+// });
+
+// Click on X to delete Todo
+$('span').click(function(event){
+    // without parent only the span is removed
+    $(this).parent().fadeOut(500, function(){
+        // refers to the li not the span
+        $(this).remove();
+    });
+    // jquery method that stops events from bubbling up to parents
+    // stops the li toggle class function from occuring
+    event.stopPropagation();
+})
