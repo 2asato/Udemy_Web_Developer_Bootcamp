653d3db725ecc6a25c179f3fd437ab3b6c0f127f
aaronsato <2asato@gmail.com>
2019-08-20 12:38:51 -0700

    repeat route complete

--- introToExpress/Assignment/app.js
+++ introToExpress/Assignment/app.js
@@ -12,22 +12,30 @@ app.get('/', function(req, res){
 // Visiting '/speak/cow' should print 'The cow says 'Moo''
 // Visiting '/speak/dog' should print 'The dog says 'Woof Woof!''
 app.get('/speak/:animalName', function(req, res){
-    var animal = req.params.animalName;
-    if (animal === 'pig'){
-        res.send("The " + animal + " says 'Oink'");
-    } else if (animal === 'cow'){
-        res.send("The " + animal + " says 'Moo'");
-    } else if (animal === 'dog'){
-        res.send("The " + animal + " says 'Woof Woof!'");
-    } else if (animal === 'horse'){
-        res.send("The " + animal + " says 'Neigh Neigh'");
-    } else if (animal === 'cat'){
-        res.send("The " + animal + " says 'Meow'");
-    } else {
-        res.send("SSSSHHHH")
+    var sounds = {
+        pig: "Oink",
+        cow: "Mooo",
+        dog: "Woof Woof!",
+        cat: "Meow",
+        goldfish: "..."
     }
+    var animal = req.params.animalName.toLowerCase();
+    var sound = sounds[animal];
+    res.send("The " + animal + " says '" + sound +"'");  
 })
 
+// Visiting '/repeat/hello/3' should print 'hello hello hello'
+// Visiting '/repeat/hello/5' should print 'hello hello hello hello hello'
+// Visiting '/repeat/blah/2' should print 'blah blah'
+app.get('/repeat/:term/:count', function(req, res){
+    var term = req.params.term;
+    var count = Number(req.params.count);
+    var string = "";
+    for (let i = 0; i <= count; i++){
+        string += term + " ";
+    }
+    res.send(string);
+})
 
 
 // Tell Express to listen for requests (start server)
