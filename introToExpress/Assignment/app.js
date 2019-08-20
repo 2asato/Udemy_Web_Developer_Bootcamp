57e326e33f8575bbf5c4c0f92ec733c40b9d7d47
aaronsato <2asato@gmail.com>
2019-08-20 11:53:52 -0700

    animal sounds done

--- introToExpress/Assignment/app.js
+++ introToExpress/Assignment/app.js
@@ -11,7 +11,22 @@ app.get('/', function(req, res){
 // Visiting '/speak/pig' should print 'The pig says 'Oink''
 // Visiting '/speak/cow' should print 'The cow says 'Moo''
 // Visiting '/speak/dog' should print 'The dog says 'Woof Woof!''
-
+app.get('/speak/:animalName', function(req, res){
+    var animal = req.params.animalName;
+    if (animal === 'pig'){
+        res.send("The " + animal + " says 'Oink'");
+    } else if (animal === 'cow'){
+        res.send("The " + animal + " says 'Moo'");
+    } else if (animal === 'dog'){
+        res.send("The " + animal + " says 'Woof Woof!'");
+    } else if (animal === 'horse'){
+        res.send("The " + animal + " says 'Neigh Neigh'");
+    } else if (animal === 'cat'){
+        res.send("The " + animal + " says 'Meow'");
+    } else {
+        res.send("SSSSHHHH")
+    }
+})
 
 
 
