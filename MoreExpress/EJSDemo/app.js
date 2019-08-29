72fdfad01f42a047470a41f13397c6b04ef6760d
aaronsato <2asato@gmail.com>
2019-08-28 22:03:04 -0700

    clean up

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
     res.render("home.ejs");
 })
 
+app.get('/fallinlovewith/:thing', function(req, res){
+    var thing = req.params.thing;
+    // passes :thing under thingVar to HTML
+    res.render("love.ejs", {thingVar: thing});
+})
 
 
 
