4d05586b40642dce113ed7f87999d3e8d47a2b03
aaronsato <2asato@gmail.com>
2019-09-10 14:37:35 -0700

    ejs conditionals and loops exercise

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -12,6 +12,15 @@ app.get('/fallinlovewith/:thing', function(req, res){
     res.render("love.ejs", {thingVar: thing});
 })
 
+app.get('/posts', function(req, res){
+    var posts = [
+        {title: 'post 1', author: 'Susy'},
+        {title: 'My adorable pet bunny', author: 'Charlie'},
+        {title: 'Can you believe this pomsky', author: 'Colt'}
+    ];
+    res.render('posts.ejs', {posts: posts})
+})
+
 
 
 
