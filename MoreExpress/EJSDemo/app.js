f0e4fa1129c986804069f575f8998f6a1b77ea83
aaronsato <2asato@gmail.com>
2019-09-11 12:25:27 -0700

    complete styles and partials exercise

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var app = express();
 
+// to use anything in public folder
+app.use(express.static('public'));
+// expects ejs files, dont have to write .ejs
+app.set('view engine', 'ejs');
+
 // '/' route
 app.get('/', function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 })
 
+// love.ejs
 app.get('/fallinlovewith/:thing', function(req, res){
     var thing = req.params.thing;
     // passes :thing under thingVar to HTML
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 })
 
+// posts.ejs
 app.get('/posts', function(req, res){
     var posts = [
         {title: 'post 1', author: 'Susy'},
         {title: 'My adorable pet bunny', author: 'Charlie'},
         {title: 'Can you believe this pomsky', author: 'Colt'}
     ];
-    res.render('posts.ejs', {posts: posts})
+    res.render('posts', {posts: posts})
 })
+
+
 
 
 
