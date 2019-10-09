ba5c698848d157387f6ed4a144a3db47a27073b4
aaronsato <2asato@gmail.com>
2019-10-09 15:55:19 -0700

    created schema and Cat model

--- databases/cats.js
+++ databases/cats.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cat_app', { useNewUrlParser: true, useUnifiedTopology: true });
 
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
 // adding a new cat to the DB
 
 
