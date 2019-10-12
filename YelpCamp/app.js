e9f0cd552f658d1d15acf2000ea5c0481bb54ce6
aaronsato <2asato@gmail.com>
2019-10-11 23:14:46 -0700

    created yelp_camp db and can add to it

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,13 +4,33 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+// campground model
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+Campground.create({
+    name: 'Granite Hill', image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'
+},function(err, campground){
+    if(err){
+        console.log(err);
+        
+    } else {
+        console.log('NEWLY CREATED CAMPGROUND: ');
+        console.log(campground);
+        
+    }
+})
 
 // campgrounds array
-var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'},
-    {name: 'Granite Hill', image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'},
-    {name: 'Mountain Goat\'s Rest', image: 'https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'}
-]
+// var campgrounds = [
+//     {name: 'Salmon Creek', image: 'https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'},
+//     {name: 'Granite Hill', image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'},
+//     {name: 'Mountain Goat\'s Rest', image: 'https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c732773d39f44c35f_340.jpg'}
+// ]
 
 mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,7 +43,9 @@ app.get('/', function(req, res) {
 
 // campground page
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds: campgrounds})
+    // Get all campgrounds from DB instead of campgrounds array
+    
+    // res.render('campgrounds', {campgrounds: campgrounds})
 })
 
 // new campground page
