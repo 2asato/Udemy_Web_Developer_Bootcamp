4b8ec2251b1f0835bfbe00e534052ed0a7a0a85d
aaronsato <2asato@gmail.com>
2019-10-17 16:02:45 -0700

    show page working and added description to form

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -65,7 +65,8 @@ app.post('/campgrounds', function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
     // create new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
