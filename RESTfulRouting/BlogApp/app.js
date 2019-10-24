cb094f18018602de1d5da495c44c810d6bd71fbd
aaronsato <2asato@gmail.com>
2019-10-24 11:12:17 -0700

    created new and create routes and form

--- RESTfulRouting/BlogApp/app.js
+++ RESTfulRouting/BlogApp/app.js
@@ -44,6 +44,23 @@ app.get('/blogs', function(req, res) {
     });
 });
 
+// new route
+app.get('/blogs/new', function(req, res) {
+    res.render('new')
+})
+
+// create route
+app.post('/blogs', function(req, res) {
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
+
 
 
 
