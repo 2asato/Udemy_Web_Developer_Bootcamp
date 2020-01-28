2a58657ce69a719a699f659bb02404d73a0c47ba
aaronsato <2asato@gmail.com>
2020-01-28 11:20:51 -0800

    refactored campground

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,16 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Campground = require('./models/campground');
 
-// schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
 
-// campground model
-var Campground = mongoose.model('Campground', campgroundSchema);
+
 
 // Campground.create({
 //     name: 'Granite Hill', 
