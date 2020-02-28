87fd31ae4b2f1af76fc29752886bd532c6181661
aaronsato <2asato@gmail.com>
2020-02-27 16:30:22 -0800

    user/campground association complete

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -19,18 +19,25 @@ router.get('/', function(req, res) {
 
 // create route - add new campground to db
 // new campground page
-router.post('/', function(req, res) {
+router.post('/', isLoggedIn, function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author:author}
+
     // create new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
             
         } else {
+            console.log(newlyCreated);
+            
             // redirect to campgrounds page
             res.redirect('/campgrounds');
 
@@ -40,7 +47,7 @@ router.post('/', function(req, res) {
 
 // new route - show form to create new campground
 // add new campground page
-router.get('/new', function(req, res) {
+router.get('/new', isLoggedIn, function(req, res) {
     res.render('campgrounds/new.ejs')
 })
 
@@ -63,5 +70,12 @@ router.use(function(req, res, next){
     res.locals.currentUser = req.user;
     next();
 })
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
